de2d35051ff1b47dd370b6199dccfff9ac38d637
aadyanair <149501018+aadyanair@users.noreply.github.com>
2024-07-04 00:53:13 +0530

    Added background color to Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar navbar-expand-lg navbar-dark" style={{backgroundColor:'#2a7fba'}}>
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/">Book a Doctor</Link>
                 <button
