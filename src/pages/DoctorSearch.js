aecf9c63cc95c3164f1d19135be877e1ebf48c58
naveen george joseph <naveengjoseph@gmail.com>
2024-07-13 02:48:04 +0530

    backend

--- src/pages/DoctorSearch.js
+++ src/pages/DoctorSearch.js
@@ -147,4 +147,37 @@ const DoctorSearch = () => {
     );
 };
 
+
+
 export default DoctorSearch;
+
+
+
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+
+// function App() {
+//   const [items, setItems] = useState([]);
+
+//   useEffect(() => {
+//     axios.get("/api/items")
+//       .then(response => setItems(response.data))
+//       .catch(error => console.error(error));
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Items</h1>
+//       <ul>
+//         {items.map(item => (
+//           <li key={item._id}>
+//             <h3>{item.name}</h3>
+//             <p>{item.description}</p>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default App;
