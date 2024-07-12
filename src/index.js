0505a72866f96d0fb1fa50ab25adc046b7b8fe55
naveen george joseph <naveengjoseph@gmail.com>
2024-07-13 02:52:20 +0530

    backend

--- src/index.js
+++ src/index.js
@@ -16,3 +16,21 @@ root.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
+import mongoose from "mongoose";
+import Blog from "./model/Blog";
+
+mongoose.connect(
+  "mongodb+srv://mongo:mongo@cluster0.eyhty.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+);
+
+// Create a new blog post object
+const article = new Blog({
+  title: "Awesome Post!",
+  slug: "awesome-post",
+  published: true,
+  content: "This is the best post ever",
+  tags: ["featured", "announcement"],
+});
+
+// Insert the article in our MongoDB database
+await article.save();
