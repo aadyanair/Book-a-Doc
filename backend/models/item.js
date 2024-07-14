a199d37ad7f4adb6bab3523bfdf9f313daa5b6dc
naveen george joseph <naveengjoseph@gmail.com>
2024-07-14 23:14:46 +0530

    backend

--- backend/models/item.js
+++ backend/models/item.js
@@ -1,9 +1,16 @@
+const { type } = require("@testing-library/user-event/dist/type");
 const mongoose = require("mongoose");
 
 const itemSchema = new mongoose.Schema({
-  speciality: String,
-  location: String,
-  doctors: String,
+  speciality: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  doctors: {
+    type: String,
+  },
 });
 
 module.exports = mongoose.model("Item", itemSchema);
